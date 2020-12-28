ddf288bb34732359beb3872eb0e4c758ef923975
RajathaDH <59072510+RajathaDH@users.noreply.github.com>
2020-12-28 15:43:47 +0530

    Added header file.

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,29 @@ const express = require('express');
 
 const router = express.Router();
 
+// send home page
 router.get('/', (req, res) => {
     res.render('index');
 });
 
+// send login page
 router.get('/login', (req, res) => {
     res.render('login');
+});
+
+// login user
+router.post('/login', (req, res) => {
+    res.send('hello');
+});
+
+// send register page
+router.get('/register', (req, res) => {
+    res.render('register');
+});
+
+// register user
+router.post('/register', (req, res) => {
+    res.send('register');
 });
 
 module.exports = router;
