4d3db49f7622253062ee5eea29c58195484eb08e
RajathaDH <59072510+RajathaDH@users.noreply.github.com>
2021-01-13 07:59:51 +0530

    Added report styles.

--- public/js/reports.js
+++ public/js/reports.js
@@ -10,3 +10,16 @@ function previewImage(event){
 function newReport(){
     
 }
+
+function openEdit(reportId){
+    const reportContent = document.getElementById('d' + reportId);
+    const reportForm = document.getElementById('f' + reportId);
+
+    if(reportForm.style.display == 'none'){
+        reportContent.style.display = 'none';
+        reportForm.style.display = 'block';
+    } else {
+        reportContent.style.display = 'block';
+        reportForm.style.display = 'none';
+    }
+}
